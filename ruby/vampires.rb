f51b15d9fb9b57cbea24a067cc4266a9206f772b
gem install rails -v 4.2.3 <bahman.sadiq@gmail.com>
2016-08-01 12:26:30 -0700

    Ask the applicant if he would like to eat garlic and enroll in company's health insurance

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -4,3 +4,8 @@ puts" How old are you?"
 given_age=gets.chomp.to_i
 puts" What year were you born?"
 year_born=gets.chomp.to_i
+
+puts" Our company cafeteria serves garlic bread. Should we order some for you? "
+user_answer=gets.chomp.to_s
+puts" Would you like to enroll in the company’s health insurance? "
+health_insurance=gets.chomp.to_s

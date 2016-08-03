f77866d753e20314d30fc500abfde5f8062f56ab
gem install rails -v 4.2.3 <bahman.sadiq@gmail.com>
2016-08-02 19:39:51 -0700

    Print the result to Clerk

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -2,11 +2,11 @@ puts "What is your hamster's name?"
 ham_name=gets.chomp
 
 while true
-puts "What is your hamster's volume level? (1 to 10)"
-ham_vol=gets.chomp.to_i
-    if   (ham_vol<= 10 && ham_vol>=0)
-      break
-	end 
+	puts "What is your hamster's volume level? (1 to 10)"
+	ham_vol=gets.chomp.to_i
+	    if   (ham_vol<= 10 && ham_vol>=0)
+	      break
+		end 
 
 end 
 	
@@ -25,3 +25,6 @@ ham_age=gets.chomp.to_i
   if ham_age==""
       ham_age=nil
    end
+   puts "Hamster name #{ham_name}, volume level #{ham_vol}, 
+fur color #{ham_fur}, adoption candidacy #{ham_candidate} and
+age is #{ham_age}"

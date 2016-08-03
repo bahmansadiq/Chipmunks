68130b5e95c94a82d7ab3649d67d7c5c33773962
bahmansadiq <bahman.sadiq@gmail.com>
2016-08-02 19:45:31 -0700

    Update hamsters.rb

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -6,7 +6,7 @@ while true
 	ham_vol=gets.chomp.to_i
 	    if   (ham_vol<= 10 && ham_vol>=0)
 	      break
-		end 
+	    end 
 
 end 
 	
@@ -17,12 +17,12 @@ while true
 	puts "Is this hamster a good candidate for adoption? Type \"yes\" or \"no\"."
 	ham_candidate = gets.chomp
 	if (ham_candidate == "yes" || ham_candidate =="no")
-		break
+	   break
 	end
 end	
 puts "How old is you hamster?"
 ham_age=gets.chomp.to_i
-  if ham_age==""
+   if ham_age==""
       ham_age=nil
    end
    puts "Hamster name #{ham_name}, volume level #{ham_vol}, 

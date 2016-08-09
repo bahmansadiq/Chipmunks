9bb377aa3a28fcc9ec2395208d93ad1c9b6cab28
gem install rails -v 4.2.3 <bahman.sadiq@gmail.com>
2016-08-08 20:10:58 -0700

    If the user choice is 'yes' make an update based on user choice

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -28,5 +28,68 @@ else
 end
 #Ask the applicant if he would like to change any information at the list
 puts"would you like to update any of the given informatio? ('yes' or 'no')"
-while true 
+
 answer=gets.chomp
+# if the answer is 'yes' aks user to make a choice
+	  if answer=="yes"
+	 	
+	 	puts"Chose the information you would like to update from the list ?"
+	 	p home_decor.each{|data, detail| puts" #{data}: #{detail}"}
+	 	choice=gets.chomp 
+	 	case 
+	 	     when choice=="name"
+	 	     puts"What is your name?"
+	 	     updated_name=gets.chomp
+	 	     home_decor[:name]=updated_name
+	 	     puts"Thank you for updating your final list is:"
+	         puts "#{home_decor.each{|data, detail| puts" #{data}: #{detail}"}}"
+	 	     
+	 	     when choice=="age"
+	 	     puts" How old are you ?"
+	 	     updated_age=gets.chomp 
+	  	     home_decor[:age]=updated_age
+	  	     puts"Thank you for updating your final list is:"
+	         puts"#{home_decor.each{|data, detail| puts" #{data}: #{detail}"}}"
+
+	  	     when choice == "pref_language"
+	  	     puts"What is your prefered language?"
+	  	     updated_lang=gets.chomp 
+	  	     home_decor[:pref_language]=updated_lang
+	  	     puts"Thank you for updating your final list is:"
+	         puts"#{home_decor.each{|data, detail| puts" #{data}: #{detail}"}}"  	 
+	         
+	         when choice=="design"
+	         puts"Do you like classic design?"
+	         updated_design=gets.chomp
+	         home_decor[:likes_classic_design]=updated_design
+	  	     puts"Thank you for updating your final list is:"
+	         puts"#{home_decor.each{|data, detail| puts" #{data}: #{detail}"}}"  
+	         
+	         when choice=="decorative_box"
+	         puts"Do you like a decorative box at the hall?"
+	         updated_dec_box=gets.chomp 
+             home_decor[:decorative_box]=updated_dec_box
+             puts"Thank you for updating your final list is:"
+	         puts"#{home_decor.each{|data, detail| puts" #{data}: #{detail}"}}"  
+	         
+	         when choice=="email"
+	         puts"What is your email address?"
+			 updated_email=gets.chomp
+			 home_decor[:email]=updated_email.to_sym
+             puts"Thank you for updating your final list is:"
+	         puts"#{home_decor.each{|data, detail| puts" #{data}: #{detail}"}}"  
+	         
+	         when choice=="phone"
+	         puts"What is your phone number?"
+		   	updated_phone=gets.chomp.to_i
+			 home_decor[:phone]=updated_phone
+			 puts"Thank you for updating your final list is:"
+	         puts"#{home_decor.each{|data, detail| puts" #{data}: #{detail}"}}" 
+
+	         else 
+	  	     puts"It looks everyting is fine!"
+	  	     
+	  	     
+	         
+	 	end 
+	 end 

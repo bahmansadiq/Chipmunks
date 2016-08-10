9635dc323951c47dafd803b92ffe632a8eeb259f
gem install rails -v 4.2.3 <bahman.sadiq@gmail.com>
2016-08-10 12:01:08 -0700

    Add a nested data structure of hash and array

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,13 @@
+zas_motor={
+				"Toyota"=> { "Corolla"=> [2001, 2003, 2010, 2015],
+							:Prius => [2001, 2003, 2010, 2015],
+							:Camry => [2001, 2003, 2010, 2015],
+							:Sentra => [2001, 2003, 2010, 2015]
+					
+			          	},
+				"Honda" =>["Civic" , "Accord", "Insight"],
+				"Hyundai"=> ["Sonata", "Elentra"],
+				"Lexus"=> "SUV",
+				"colors" =>["white", "black", "pink"," Silver"],
+				"price"=>{ }
+}

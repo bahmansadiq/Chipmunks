4d21e376cf5f6cd090512825cb376af1a9be4634
gem install rails -v 4.2.3 <bahman.sadiq@gmail.com>
2016-08-10 12:41:20 -0700

    Get user first and last names and swap them

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -1,5 +1,16 @@
 #Ask the user for first and last name
 #convert them to arrays 
 #Swap first and last name using array method
+#then store them in new variable 
 #Declare a list of vowels and check the names if they include vowels and forward them one letter ahead in vowels only
 #Declare list of consonants and then check the rest of letters in consonant and forward them one letter ahead in consonants only.
+puts"What is your first name?"
+first_name=gets.chomp.chars
+puts"What is your last name?"
+last_name=gets.chomp.chars
+
+names=[]
+names.push(first_name, last_name)
+
+full_name= names.last.join("") + " " + names.first.join("")
+puts"yout full name after swapping is: #{full_name}"

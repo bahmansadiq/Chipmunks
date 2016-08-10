5b705869281a3747ce503a3a8debddd4e7d37a59
bahmansadiq <bahman.sadiq@gmail.com>
2016-08-10 12:11:10 -0700

    Update nested_data_structures.rb

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -1,15 +1,14 @@
 zas_motor={
-				"Toyota"=> { "Corolla"=> [2001, 2003, 2010, 2015],
-							:Prius => [2001, 2003, 2010, 2015],
-							:Camry => [2001, 2003, 2010, 2015],
-							:Sentra => [2001, 2003, 2010, 2015]
-					
-			          	},
-				"Honda" =>["Civic" , "Accord", "Insight"],
-				"Hyundai"=> ["Sonata", "Elentra"],
-				"Lexus"=> "SUV",
-				"colors" =>["white", "black", "pink"," Silver"],
-				"price"=>{ }
+	   "Toyota"=> { "Corolla"=> [2001, 2003, 2010, 2015],
+			:Prius => [2001, 2003, 2010, 2015],
+			:Camry => [2001, 2003, 2010, 2015],
+			:Sentra => [2001, 2003, 2010, 2015]
+		      },
+	    "Honda" =>["Civic" , "Accord", "Insight"],
+	    "Hyundai"=> ["Sonata", "Elentra"],
+	    "Lexus"=> "SUV",
+	    "colors" =>["white", "black", "pink"," Silver"],
+	    "price"=>{ }
          }
 #To print specific nested data structure in zas_motor 
 puts"Zas Motor has Toyota Corolla years: #{zas_motor["Toyota"]["Corolla"]} available!"

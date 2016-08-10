48127be58747bf079fe2bc9d148e4c21cf6de8b1
gem install rails -v 4.2.3 <bahman.sadiq@gmail.com>
2016-08-10 12:24:41 -0700

    Pseudocode alias manager

---
+++ ruby/alias_manager.rb
@@ -0,0 +1,5 @@
+#Ask the user for first and last name
+#convert them to arrays 
+#Swap first and last name using array method
+#Declare a list of vowels and check the names if they include vowels and forward them one letter ahead in vowels only
+#Declare list of consonants and then check the rest of letters in consonant and forward them one letter ahead in consonants only.

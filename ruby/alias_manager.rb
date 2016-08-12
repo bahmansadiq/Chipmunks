a4074d00a564c5a0c493341aba62786438ad2498
gem install rails -v 4.2.3 <bahman.sadiq@gmail.com>
2016-08-11 23:25:39 -0700

    Def method to swap first and last names

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -4,13 +4,26 @@
 #then store them in new variable 
 #Declare a list of vowels and check the names if they include vowels and forward them one letter ahead in vowels only
 #Declare list of consonants and then check the rest of letters in consonant and forward them one letter ahead in consonants only.
-puts"What is your first name?"
-first_name=gets.chomp.chars
-puts"What is your last name?"
-last_name=gets.chomp.chars
 
-names=[]
-names.push(first_name, last_name)
 
-full_name= names.last.join("") + " " + names.first.join("")
-puts"yout full name after swapping is: #{full_name}"
+
+def swapnames(name)
+vowels="aeioua"
+consonant= "bcdfghjklmnpqrstvwxyz"
+answer =""
+split_names=name.downcase.split(" ")
+swap_names=split_names.last + ' ' + split_names.first 
+out =""
+swap_names.chars.each do |character|
+	if vowels.include?(character)
+	out += encrypt_vowels(character)
+	
+	elsif consonant.include?(character)
+	out +=  encryp_consonat(character)
+
+    else
+	out += character
+    end 
+	
+	
+end 

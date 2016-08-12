d718084f23812209d58dbbda25bd7b4165fb1a82
gem install rails -v 4.2.3 <bahman.sadiq@gmail.com>
2016-08-11 23:26:39 -0700

    Def method to encrypt vowels one letter ahead

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -5,7 +5,11 @@
 #Declare a list of vowels and check the names if they include vowels and forward them one letter ahead in vowels only
 #Declare list of consonants and then check the rest of letters in consonant and forward them one letter ahead in consonants only.
 
-
+def encrypt_vowels(vowel)
+	vowels="aeioua"
+	vowels_out=""
+	vowels_out += vowels[vowels.index(vowel)+1]
+end 
 
 def swapnames(name)
 vowels="aeioua"

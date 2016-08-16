9107cc64af8a9b67b527f5d4be5c476a49de6b9a
gem install rails -v 4.2.3 <bahman.sadiq@gmail.com>
2016-08-16 15:34:50 -0700

    Write my own method to have intialize and two other methods

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -11,17 +11,17 @@ class Puppy
 	def speak(num)
 		num.to_i.times{puts("woof!")}
 	end 
-	
+
 	def roll_over
 		puts"*roll_over"
 	end
-	
+
 	def dog_years(int)
 		human_year=int.to_i
 		dog_year= human_year * 10.5
 		puts" a dog ages #{dog_year} years for human year #{human_year}"
 	end 
-	
+
 	def cool_dog(name, jump)
 		puts"My dog #{name} jumps #{jump} feets high"
 	end 
@@ -33,3 +33,33 @@ puppy.speak(10)
 puppy.roll_over
 puppy.dog_years(10)
 puppy.cool_dog("zack", 6)
+
+
+#My own method 
+
+class Myclass
+	def initialize
+		puts" initialized method runs at the beginning"
+	end 
+	
+	def katty(name)
+		puts"Hi, #{name}"
+		
+   end 
+    
+    def bob(name, age)
+    	puts"#{name} is #{age} years old!"
+    end 
+end 
+
+
+storage=[]
+
+for i in 1..50
+ storage << Myclass.new 
+end 
+
+storage.each do|items|
+	 items.katty("John Marthon")
+    items.bob("Zack", 34)
+end 

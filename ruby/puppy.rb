2cda4444adba6fa9c6afab15fda73a793f33fee4
gem install rails -v 4.2.3 <bahman.sadiq@gmail.com>
2016-08-16 13:18:46 -0700

    Add characteristics to puppy

---
+++ ruby/puppy.rb
@@ -0,0 +1,13 @@
+
+#characteristics:
+
+#number of legs: 4
+#number of eyes: 2
+#frindly : yes
+# Does it eat vegetables : no
+#Can be adopted as a pet: yes
+#color varies: yes
+#Minimum weight: 2 lbs
+#Maximum Weight : 20 lbs
+# does it have nails: yes
+#Exterior Hears:yes

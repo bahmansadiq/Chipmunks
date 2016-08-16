eb21fb8628aac36f44359f38f39e3d11efb19a6b
gem install rails -v 4.2.3 <bahman.sadiq@gmail.com>
2016-08-16 14:05:25 -0700

    Initialize a method that takes human year and return dog year

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -6,14 +6,22 @@ class Puppy
   end
   def speak(num)
   	num.to_i.times{puts("woof!")}
-  end
-
-  def roll_over
-    puts"*roll_over"
   end 
- 
+  
+  def roll_over
+   puts"*roll_over"
+  end
+  
+  def dog_years(int)
+  	human_year=int.to_i
+    dog_year= human_year * 10.5
+    puts" a dog ages #{dog_year} years for human year #{human_year}"
+   end 
+
 end
 
 puppy=Puppy.new
 puppy.fetch("ball")
 puppy.speak(10)
+puppy.roll_over
+puppy.dog_years(10)

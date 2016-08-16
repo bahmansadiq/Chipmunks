0807537a098eb1e23b35999422a3686c3e2305b8
gem install rails -v 4.2.3 <bahman.sadiq@gmail.com>
2016-08-16 14:09:18 -0700

    Add cool dog feature and driver code

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -14,10 +14,13 @@ class Puppy
   
   def dog_years(int)
   	human_year=int.to_i
-    dog_year= human_year * 10.5
-    puts" a dog ages #{dog_year} years for human year #{human_year}"
+   dog_year= human_year * 10.5
+   puts" a dog ages #{dog_year} years for human year #{human_year}"
    end 
-
+   
+   def cool_dog(name, jump)
+   	 puts"My dog #{name} jumps #{jump} feets high"
+	end 
 end
 
 puppy=Puppy.new
@@ -25,3 +28,4 @@ puppy.fetch("ball")
 puppy.speak(10)
 puppy.roll_over
 puppy.dog_years(10)
+puppy.cool_dog("zack", 6)

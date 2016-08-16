70736135e72a52804ef25b43c468a0515e6d6f83
gem install rails -v 4.2.3 <bahman.sadiq@gmail.com>
2016-08-16 14:18:01 -0700

    Add Initialize method

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,25 +1,29 @@
 class Puppy
 
-  def fetch(toy)
-    puts "I brought back the #{toy}!"
-    toy
-  end
-  def speak(num)
-  	num.to_i.times{puts("woof!")}
-  end 
-  
-  def roll_over
-   puts"*roll_over"
-  end
-  
-  def dog_years(int)
-  	human_year=int.to_i
-   dog_year= human_year * 10.5
-   puts" a dog ages #{dog_year} years for human year #{human_year}"
-   end 
-   
-   def cool_dog(name, jump)
-   	 puts"My dog #{name} jumps #{jump} feets high"
+	def initialize
+		puts"Initializing new puppy instance ..."
+	end 
+
+	def fetch(toy)
+		puts "I brought back the #{toy}!"
+		toy
+	end
+	def speak(num)
+		num.to_i.times{puts("woof!")}
+	end 
+	
+	def roll_over
+		puts"*roll_over"
+	end
+	
+	def dog_years(int)
+		human_year=int.to_i
+		dog_year= human_year * 10.5
+		puts" a dog ages #{dog_year} years for human year #{human_year}"
+	end 
+	
+	def cool_dog(name, jump)
+		puts"My dog #{name} jumps #{jump} feets high"
 	end 
 end
 

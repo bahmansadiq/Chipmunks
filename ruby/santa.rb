0935b3cce6fb742f1e829be9fcaca73779baeadc
gem install rails -v 4.2.3 <bahman.sadiq@gmail.com>
2016-08-16 22:12:06 -0700

    Add get_mad_at and celebrate_birthday instance methods

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -2,7 +2,7 @@ class	Santa
 	def initialize(name, gender)
 		@name=name 
 		@gender=gender
-		reindeer_ranking=["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+		@reindeer_ranking=["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
 		@age=0
 		puts"Initializing Santa instance ..."
 	end
@@ -15,8 +15,18 @@ class	Santa
 		puts"That was a good #{cooky_type}!"
 	end 
 	
+	def celebrate_birthday(age)
+		@age= age + 1
+		puts"Happy #{@age} birthday "
+	end 
+	def get_mad_at(name)
+		@reindeer_ranking.push(name)
+		p @reindeer_ranking
+	end 
 	def about
 		puts"name #{@name} and gender #{@gender}"
+		puts" reindeer_ranking new list"
+		puts"#{@reindeer_ranking}"
 	end 
 
 end
@@ -24,6 +34,8 @@ end
 santa=Santa.new("Nazik", "Shemale")
 santa.speak
 santa.eat_milk_and_cookies("kitcat")
+santa.celebrate_birthday(12)
+santa.get_mad_at("Hasti")
 santa.about
 
 
@@ -31,5 +43,5 @@ santas = []
 example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
 example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
 example_ethnicities.length.times do |i|
-	santas << Santa.new(example_genders[i], example_ethnicities[i])
+  santas << Santa.new(example_genders[i], example_ethnicities[i])
 end

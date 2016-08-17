e5b1b6b1b73e3642ae3051a95470c249f37ec2b2
gem install rails -v 4.2.3 <bahman.sadiq@gmail.com>
2016-08-17 00:01:28 -0700

    Randomly Select name, age and gender for all santas

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -7,6 +7,7 @@ class	Santa
 		@reindeer_ranking=["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
 		@age=0
 		@ethnicity="ethnicity"
+		
 	end
 	
 	def speak
@@ -19,6 +20,7 @@ class	Santa
 	
 	def celebrate_birthday(age)
 		@age= age + 1
+		
 		puts"Happy #{@age} birthday"
 	end 
 	def get_mad_at(name)
@@ -47,17 +49,28 @@ end
 santa=Santa.new("Nazik", "Shemale")
 santa.speak
 santa.eat_milk_and_cookies("kitcat")
-santa.celebrate_birthday(12)
+mychoice=Random.new
+nom=mychoice.rand(0..140)
+santa.celebrate_birthday(nom)
 santa.get_mad_at("Hasti")
 santa.gender="Bichol"
 p santa.ethnicity
 p santa.age 
-santa.about
+p santa.about
 
 
+puts"list of santas"
 santas = []
-example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
-example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
-example_ethnicities.length.times do |i|
+example_genders = ["agender", "female", "bigender", "male", "female", "Shemale", "gender fluid", "N/A"]
+example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "Albanians", "Persians", "N/A"]
+len=example_ethnicities.length 
+
+for i in 1..len
 	santas << Santa.new(example_genders[i], example_ethnicities[i])
+
 end
+rand_select=[]
+rand_select << Santa.new(example_ethnicities.sample, example_genders.sample)
+
+ 
+

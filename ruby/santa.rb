d576301e4ac22947036ef06b18134566c2ae95b5
gem install rails -v 4.2.3 <bahman.sadiq@gmail.com>
2016-08-16 21:04:19 -0700

    Pass two arrays to class intialize

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,5 +1,9 @@
 class	Santa
-	def initialize
+	def initialize(name, gender)
+		@name=name 
+		@gender=gender
+		reindeer_ranking=["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+		@age=0
 		puts"Initializing Santa instance ..."
 	end
 	
@@ -16,3 +20,11 @@ end
 santa=Santa.new 
 santa.speak
 santa.eat_milk_and_cookies("kitcat")
+
+
+santas = []
+example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
+example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
+example_ethnicities.length.times do |i|
+  santas << Santa.new(example_genders[i], example_ethnicities[i])
+end

e7a5b245b436de46bd1f4db60a9e3852497a4ebb
gem install rails -v 4.2.3 <bahman.sadiq@gmail.com>
2016-08-16 21:30:04 -0700

    Add diverse driver test and about method

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -14,17 +14,22 @@ class	Santa
 	def eat_milk_and_cookies(cooky_type)
 		puts"That was a good #{cooky_type}!"
 	end 
+	
+	def about
+		puts"name #{@name} and gender #{@gender}"
+	end 
 
 end
 
-santa=Santa.new 
+santa=Santa.new("Nazik", "Shemale")
 santa.speak
 santa.eat_milk_and_cookies("kitcat")
+santa.about
 
 
 santas = []
 example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
 example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
 example_ethnicities.length.times do |i|
-  santas << Santa.new(example_genders[i], example_ethnicities[i])
+	santas << Santa.new(example_genders[i], example_ethnicities[i])
 end

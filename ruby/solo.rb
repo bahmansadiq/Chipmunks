80c23233d217cf745f706c1f57412b22061075e4
gem install rails -v 4.2.3 <bahman.sadiq@gmail.com>
2016-08-17 17:24:35 -0700

    Define a method to direct inputs to be evaluated inside other methods

--- ruby/solo.rb
+++ ruby/solo.rb
@@ -5,15 +5,32 @@ class Survey
 		@year=year
 
 	end 
+
+
 	def first_grades(grade)
 		puts"Congratulations!!!!!!!!!!"
-		puts" Student #{@name} with #{@percentage}% has achieved A+ grade" 
+		puts" Student #{@name} with #{@percentage} percents has achieved A+ grade" 
 		
 	end 
 	
 	def second_grades(grade)
 		puts"Congratulations!!!!!!!!!!"
-		puts" Student #{@name} with #{@percentage}% has achieved B+ grade" 
+		puts" Student #{@name} with #{@percentage} percents has achieved B+ grade" 
 	end 
 	
+	def students_survery	
+		if @percentage.to_i > 70
+			first_grades(@percentage)
+
+		elsif @percentage.to_i > 60
+			second_grades(@percentage)
+		else 
+			puts" Sorry #{@name} you have failed #{@year} summer semester getting #{@percentage} percents on the test"
+		end 
+	end 
+
+
 end 
+
+survey=Survey.new("Bahman", 69, 2016)
+survey.students_survery

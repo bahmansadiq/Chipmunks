81b6f8218fdc2bd6b420e8f4be8f5710f8b7c185
gem install rails -v 4.2.3 <bahman.sadiq@gmail.com>
2016-08-18 15:35:34 -0700

    Store the result in an array datatype and go through array to print the attributes of each instance

--- ruby/solo.rb
+++ ruby/solo.rb
@@ -7,12 +7,13 @@
 
 class Survey
 
-	attr_reader :percentage, :year
+	attr_reader :percentage, :year, :out
 	attr_accessor :name
 	def initialize(name, percentage, year)
 		@name=name
 		@percentage=percentage
 		@year=year
+		@out=[]
 
 	end 
 
@@ -20,43 +21,59 @@ class Survey
 	def first_grades(grade)
 		puts"Congratulations!!!!!!!!!!"
 		puts" Student #{@name} with #{@percentage} percents has achieved A+ grade!" 
-		
+		grade
+
+
 	end 
 	
 	def second_grades(grade)
 		puts"Congratulations!!!!!!!!!!"
 		puts" Student #{@name} with #{@percentage} percents has achieved B+ grade!" 
+		grade
+
 	end 
 	#Get the student information from initialize method	
 	def students_survery	
-	#if the percentage is more than 70 send the percentage as an argument to first_grades method
-	if @percentage.to_i > 70
-		first_grades(@percentage)
+		#if the percentage is more than 70 send the percentage as an argument to first_grades method
+		if @percentage.to_i >= 70
+			@out.push(@name, @year, first_grades(@percentage), "A")
 
-	elsif @percentage.to_i > 60
-		#if the percentage is more than 60 send the percentage as an argument to second_grades method
-		second_grades(@percentage)
-		#otherwise print the following statments.
-	else 
-		puts" Sorry #{@name} you have failed #{@year} summer semester getting #{@percentage} percents on the final!"
+		elsif @percentage.to_i >= 60 
+			#if the percentage is more than 60 send the percentage as an argument to second_grades method
+			@out.push(@name, @year, second_grades(@percentage), "B")
+			#otherwise print the following statments.
+		else 
+			@out.push(@name, @year, @percentage,"failed!")
+			
+		end 
+
 	end 
-	end 
+
 
 
 end 
-result=[]
+survey=[]
+#or i can leave survey to be a string an avoid line 55
+list_of_students=[]
+
 while true 
-puts"Please provide your name or type 'done' to exit?"
-name=gets.chomp
-break if name=="done"
-puts"What is your score on the final test?"
-score=gets.chomp.to_f
-puts"Which year's result would you like to access?"
-year=gets.chomp.to_i 
-result << Survey.new(name, score, year)
+	puts"Please provide your name or type 'done' to exit?"
+	name=gets.chomp
+	break if name=="done"
+	puts"What is your score on the final test?"
+	score=gets.chomp.to_f
+	puts"Which year's result would you like to access?"
+	year=gets.chomp.to_i 
+	survey = Survey.new(name, score, year)
+	list_of_students << survey.students_survery
+end
 #Driver code to over write name attribute with attr_accessor
 #survey.name="Jose"
 #Driver code to read year attribute with attr_reader
 #survey.year
-result << result.students_survery
+puts"Students name, year attended, percentage and grades ara:"
+puts"\n"
+list_of_students.each do |item|
+	puts item
+	puts"\n\n"
 end 

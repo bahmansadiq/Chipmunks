f688359bb35c3aa3d8c0639ab7d585ef24cb7803
gem install rails -v 4.2.3 <bahman.sadiq@gmail.com>
2016-08-17 17:38:42 -0700

    Add comments for each methods

--- ruby/solo.rb
+++ ruby/solo.rb
@@ -1,3 +1,10 @@
+#I would like to work on a students grading survey system
+#need to collect student information at the beginning 
+#Based on the information given 
+#Set condition to different grades
+#Use a method to set conditions and send the user input to other methods to show resul
+#for every grades(A, B or fail) we write a method 
+
 class Survey
 	def initialize(name, percentage, year)
 		@name=name
@@ -9,24 +16,27 @@ class Survey
 
 	def first_grades(grade)
 		puts"Congratulations!!!!!!!!!!"
-		puts" Student #{@name} with #{@percentage} percents has achieved A+ grade" 
+		puts" Student #{@name} with #{@percentage} percents has achieved A+ grade!" 
 		
 	end 
 	
 	def second_grades(grade)
 		puts"Congratulations!!!!!!!!!!"
-		puts" Student #{@name} with #{@percentage} percents has achieved B+ grade" 
+		puts" Student #{@name} with #{@percentage} percents has achieved B+ grade!" 
 	end 
-	
+	#Get the student information from initialize method	
 	def students_survery	
-		if @percentage.to_i > 70
-			first_grades(@percentage)
+	#if the percentage is more than 70 send the percentage as an argument to first_grades method
+	if @percentage.to_i > 70
+		first_grades(@percentage)
 
-		elsif @percentage.to_i > 60
-			second_grades(@percentage)
-		else 
-			puts" Sorry #{@name} you have failed #{@year} summer semester getting #{@percentage} percents on the test"
-		end 
+	elsif @percentage.to_i > 60
+		#if the percentage is more than 60 send the percentage as an argument to second_grades method
+		second_grades(@percentage)
+		#otherwise print the following statments.
+	else 
+		puts" Sorry #{@name} you have failed #{@year} summer semester getting #{@percentage} percents on the final!"
+	end 
 	end 
 
 

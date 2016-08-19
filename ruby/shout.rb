2535d7f069227203ffb7503535da44b28fbc0c0f
gem install rails -v 4.2.3 <bahman.sadiq@gmail.com>
2016-08-18 18:49:30 -0700

    Define module and methods with self keyword and test with driver code beneath

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,3 +1,13 @@
 module Shout
-	
+	  def self.yell_angrily(words)
+    words + "!!!" + " :("
+  end
+	def self.yelling_happily(noise)
+		noise
+	end 
 end
+
+
+Shout.yell_angrily("I am sad")
+
+Shout.yelling_happily("I am happy")

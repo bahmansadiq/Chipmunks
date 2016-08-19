5f7d9343e48711c90404acad17ffa32a926e91f4
gem install rails -v 4.2.3 <bahman.sadiq@gmail.com>
2016-08-18 19:09:47 -0700

    Add two classes and include the module Shout

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,13 +1,31 @@
 module Shout
-	  def self.yell_angrily(words)
-    words + "!!!" + " :("
-  end
-	def self.yelling_happily(noise)
+	  def yell_angrily(words)
+         words + "!!!" + " :("
+       end
+	def yelling_happily(noise)
 		noise
 	end 
 end
 
+class Leader
+	include Shout
+end 
 
-Shout.yell_angrily("I am sad")
+class Manager
+	include Shout
+end 
 
-Shout.yelling_happily("I am happy")
+
+leader=Leader.new 
+leader.yelling_happily("You did a good job Weldone")
+leader.yell_angrily("fired!!!")
+
+manager=Manager.new 
+manager.yell_angrily("You are not doing good, fired!!!")
+
+
+
+
+#Driver code for Shout module
+#Shout.yell_angrily("I am sad")
+#Shout.yelling_happily("I am happy")

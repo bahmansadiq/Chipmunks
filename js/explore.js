37b0392a1fa73caff7bcd228614aaaea35354bc8
gem install rails -v 4.2.3 <bahman.sadiq@gmail.com>
2016-08-24 00:23:52 -0700

    Add function to reverse the a given name or string

--- js/explore.js
+++ js/explore.js
@@ -4,3 +4,16 @@
 // go through the new variable using for loop and return the new varibale
 // call the function using console.log(); passing an argument to the function.
 
+function reverse(name){
+	var reverseName="";
+	var len=name.length -1; 
+	
+	for (var i= len; i>=0; i--) {
+	reverseName += name[i];
+
+	}
+	console.log(reverseName);
+   return reverseName;
+}
+reverse("hello");
+// output: olleh

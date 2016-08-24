1682bc9ade1d28893a65c4c2e102a34c740154d9
gem install rails -v 4.2.3 <bahman.sadiq@gmail.com>
2016-08-24 00:21:15 -0700

    Add pseudocode for reverse function

---
+++ js/explore.js
@@ -0,0 +1,6 @@
+// declare a function to take a parameter 
+// using for loop go through the parameter, find the order the have entered 
+// store them in a new variable as last come first stored
+// go through the new variable using for loop and return the new varibale
+// call the function using console.log(); passing an argument to the function.
+

d7c6363c509837db7dcecc3c0fe9d0204122cbd4
gem install rails -v 4.2.3 <bahman.sadiq@gmail.com>
2016-08-25 12:11:34 -0700

    Pseudocode how to find the longest word in an array

---
+++ js/algos.js
@@ -0,0 +1,8 @@
+// define a function which takes an array as a parameter
+// loop inside the array element and compare the lengths
+// assuming the index 0 is the longest and compare it to the next one
+// if the next index (index 1) is longer take that and compare it with the next one
+// continue this process till the end of the array and return the result 
+
+
+//["long phrase","longest phrase","longer phrase"]= "longest phrase"

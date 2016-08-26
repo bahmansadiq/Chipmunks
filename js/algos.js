f603e001f8d7e1f93c1c9eeb0c0e4a1d6c4610c2
gem install rails -v 4.2.3 <bahman.sadiq@gmail.com>
2016-08-26 01:05:00 -0700

    Add the result from ListOfStrings to longestword function

--- js/algos.js
+++ js/algos.js
@@ -23,11 +23,44 @@ function comp(para1, para2){
 	}
 	comp(list1, list2);
 
+// RELEASE 2:
+//Write a function that takes an integer for length, and builds and returns an array of strings of the given length. 
+// I have defined two functions one takes a length for the string and return a random string of the given length
+
+function randomString() {
+	var text = '';
+	var words = "abcdefghijklmnopqrstuvwxyz";
+	var randomLength= Math.floor(Math.random() * 10 +1);
+	for(var i = 0; i < randomLength; i++) {
+   // text is going to have diffrent or random set of character 
+   // each time it runs using Math function and store it inside the text variable. 	
+   text += words.charAt(Math.floor(Math.random() * words.length));
+}
+    // Return text
+    return text;
+}
+
+// another function to use the first function and create fixed length array of random strings.
+function listOfStrings(number){
+	var	stor_random=[];	
+//for loop runs according the number passed by the function.
+for(var j=0; j< number; j++)
+{
+  // call the first function and store the random strings inside the stor_random array; 	
+  stor_random.push(randomString());
+  // I can upper case the string and print or    
+  //   console.log(stor_random[j].charAt(0).toUpperCase() + stor_random[j].slice(1).toLowerCase() + " ");
+}
+   // Just simply return the array.
+   return  stor_random;
+}
+// I am passing 10 to the function which can be asked from the user too.
+console.log(listOfStrings(10));
+// store the result in an array for the next release;
+var randomArray=listOfStrings(10)
 
 
-
-
-
+//RELEASE 0:
 // define a function which takes an array as a parameter
 // loop inside the array element and compare the lengths
 // assuming the index 0 is the longest and compare it to the next one
@@ -36,8 +69,10 @@ function comp(para1, para2){
 
 
 //["long phrase","longest phrase","longer phrase"]= "longest phrase"
+var myArray=["long phrase","longest phrase","loger phrase"];
+//concat two arrays and store in the third array.
 
-var incoming=["long phrase","longest phrase","loger phrase"] 
+var incoming=myArray.concat(randomArray);
 
 
 function longestword(incoming){
@@ -53,41 +88,5 @@ function longestword(incoming){
 	}
 	return maxword;
 }
+console.log("The longest pharase is:")
 console.log(longestword(incoming));
-
-//output: "longest phrase"
-
-
-//Write a function that takes an integer for length, and builds and returns an array of strings of the given length. 
-// I have defined two functions one takes a length for the string and return a random string of the given length
-
-function randomString(str) {
-	var text = '';
-	var words = "abcdefghijklmnopqrstuvwxyz";
-	for(var i = 0; i < str; i++) {
-   // text is going to have diffrent or random set of character depends on str passing in randomString
-   // each time it runs using Math function and store it inside the text. 	
-   text += words.charAt(Math.floor(Math.random() * words.length));
-}
-    // Return text 
-    return text;
-}
-
-// another function to use the first function and create fixed length array of random strings.
-function listOfStrings(number){
-	var	stor_random=[];	
-//for loop runs according the number passed by the function.
-for(var j=0; j< number; j++)
-{
-  // call the first function and store the random strings inside the stor_random array; 	
-  stor_random.push(randomString(strLength));
-  // I can upper case the string and print or    
-  //   console.log(stor_random[j].charAt(0).toUpperCase() + stor_random[j].slice(1).toLowerCase() + " ");
-}
-   // Just simply return the array.
-   return  stor_random;
-}
-var strLength=prompt("Enter string length")
-randomString(strLength);
-var number=prompt("Enter an array length!");
-console.log(listOfStrings(number));
